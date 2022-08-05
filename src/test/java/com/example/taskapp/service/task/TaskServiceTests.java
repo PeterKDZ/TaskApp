a0f03ac4df6508773a553d528d6df4fbb57eab39
PeterKDZ <piotr.kedzierski84@gmail.com>
2022-08-05 14:48:19 +0200

    TaskSerwice exception test

--- src/test/java/com/example/taskapp/service/task/TaskServiceTests.java
+++ src/test/java/com/example/taskapp/service/task/TaskServiceTests.java
@@ -1,6 +1,7 @@
 package com.example.taskapp.service.task;
 
 import com.example.taskapp.dto.task.TaskDto;
+import com.example.taskapp.entity.task.Task;
 import com.example.taskapp.enums.task.Priority;
 import com.example.taskapp.enums.task.Status;
 import com.example.taskapp.exception.TaskException;
@@ -15,8 +16,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTests {
@@ -28,66 +31,53 @@ class TaskServiceTests {
     @Mock
     TaskRepository taskRepository;
 
-    @Before
-    public void init() {
-        TaskDto taskDto = TaskDto.builder().
-                id(2L).
-                name("test").
-                status(Status.DONE).
-                priority(Priority.HIGH).
-                description("New test task").
-                build();
-        given(taskRepository.existsTaskByNameAndStatus(taskDto.getName(), taskDto.getStatus())).willReturn(true);
-    }
-
     @Test
     @DisplayName("Testing creation new TaskEntity and saving to DB - Positive")
     void createTask_shouldCreateNewTask() {
         //given
-        TaskDto taskDto = TaskDto.builder().
-                id(2L).
+        TaskDto input = TaskDto.builder().
                 name("test").
                 status(Status.DONE).
                 priority(Priority.HIGH).
                 description("New test task").
                 build();
-        //given(taskService.createTask(taskDto)).willReturn(preparedTaskDtoTest1);
+        Task task = Task.builder().build();
+        TaskDto taskDto = TaskDto.builder().build();
+
+        when(taskMapper.convertDtoToTask(any(TaskDto.class))).thenReturn(task);
+        when(taskRepository.existsTaskByNameAndStatus(anyString(), any(Status.class))).thenReturn(false);
+        when(taskMapper.convertTaskToDto(any(Task.class))).thenReturn(taskDto);
+
         //when
-        when(taskService.createTask(taskDto)).thenReturn(preparedTaskDtoTest1);
-        TaskDto taskDtoSaved = taskService.createTask(taskDto);
+        TaskDto result = taskService.createTask(input);
 
         //then
-        assertEquals(preparedTaskDtoTest1, taskDtoSaved);
+        verify(taskRepository, times(1)).save(task);
+        assertEquals(taskDto, result);
     }
 
     @Test
-    @DisplayName("Testing creation new TaskEntity existing in DB - Exception")
+    @DisplayName("Testing creation new TaskEntity like existing in DB - Exception")
     void createTask_shouldThrowNewTaskException() {
         //given
-        TaskDto taskDtoTest2 = TaskDto.builder().
-                id(2L).
+        TaskDto input = TaskDto.builder().
                 name("test").
                 status(Status.DONE).
-                priority(Priority.HIGH).
-                description("New test task").
                 build();
+        Task task = Task.builder().build();
+        String expectedMessage = "Task with name " + input.getName() +
+                " and status " + input.getStatus() + " is already exist";
+
+        when(taskMapper.convertDtoToTask(any(TaskDto.class))).thenReturn(task);
+        when(taskRepository.existsTaskByNameAndStatus(anyString(), any(Status.class))).thenReturn(true);
 
         //when
-        when(taskService.createTask(taskDtoTest2)).thenThrow(new TaskException("Task with name " + taskDtoTest2.getName() +
-                " and status " + taskDtoTest2.getStatus() + " is already exist"));
-        TaskDto taskDtoSaved = taskService.createTask(taskDtoTest2);
+        TaskException taskException = assertThrows(TaskException.class, () -> taskService.createTask(input), expectedMessage);
+        String actualMessage = taskException.getMessage();
 
         //then
-        //assertThrows(TaskException, new TaskException("task"));
-
+        verify(taskRepository, times(1)).existsTaskByNameAndStatus(input.getName(), input.getStatus());
+        assertTrue(actualMessage.contains(expectedMessage));
     }
-
-    TaskDto preparedTaskDtoTest1 = TaskDto.builder().
-            id(2L).
-            name("First").
-            status(Status.DONE).
-            priority(Priority.HIGH).
-            description("New test task").
-            build();
 
 }
